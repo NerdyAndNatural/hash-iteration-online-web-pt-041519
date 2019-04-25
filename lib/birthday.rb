6cf7d7f880ace2d6c72d2e7bcd497dd474bd3ef5
Sarah Williams <srwilliams8678@gmail.com>
2019-04-25 01:26:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,8 @@ end
 def age_appropriate_birthday (kids_name, age)
   if age > 12 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  else puts "You are too old for this."
+  else 
+    puts "You are too old for this."
 end
 end
 
